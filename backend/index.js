66bda491d7b148189ecea1e8af255a76bd7da611
Thabendra <sathiendrathabendra@gmail.com>
2025-06-05 16:29:55 +0530

    feat:Authentication completed backend

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,22 @@
-// app.js
-import express from 'express';
-import dotenv from 'dotenv';
-import pool from './db.js'; // import the database connection
+const express = require('express');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes.js');
+const db = require('./models');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());
+app.use(express.json()); // Add this to parse JSON request bodies
+app.use('/api/auth', authRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
-// Optional: route that queries the database
-app.get('/time', async (req, res) => {
-    try {
-        const result = await pool.query('SELECT NOW()');
-        res.json({ db_time: result.rows[0].now });
-    } catch (err) {
-        res.status(500).json({ error: 'Database query failed', details: err.message });
-    }
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+db.sequelize.sync()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
---
+++ backend/models/user.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  }, {
+    timestamps: false 
+  });
+
+  return User;
+};
